c4a0e96817e172d028e6007ee5b3315e252b03c1
Joaquim Barreto <joaquim@barreto.co>
2018-10-28 19:22:16 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  = {
+    "car" => 2,
+    "house" => 1,
+    
+  }
   
 end
 
